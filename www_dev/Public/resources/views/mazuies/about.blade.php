d7054632151a86e01828ec2b97ce4021d5eddb99
鍛治多依子 <kajikoo929@gmail.com>
2019-06-04 09:12:54 +0800

    viewsファイル追加

--- www_dev/Public/resources/views/mazuies/about.blade.php
+++ www_dev/Public/resources/views/mazuies/about.blade.php
@@ -21,7 +21,7 @@
 <body>
     <div class="page-wrapper">
         <div class="bg">
-            <div class="container text-center mt-3">
+            <div class="container text-center mt-2">
                 <div>
                    <img src="imgs/aaron-blanco-tejedor-768029-unsplash.jpg" style="height: 350px;">
                 </div>
@@ -36,9 +36,13 @@
                     </div>
                </div>
                <div class="title text-center">
-                    <div>
+                    <div class="textarea">
                         <p class="text-en"><span style="border-bottom: 1px solid #808080;">About Our Website</span><br>Have you been stressed lately? Our website is for blowing off stream.<br>We focus on dark side inside people. Generally, people don’t speak ill of something (...do they?), and try not to do it even if they want. Thus, our website is for the people who want to have fun by making fun of something.<br>Let's vent your dirt! Then, be refreshed & be fresh!</p>
+                        <p class="text-en">※We do not take responsibility or liability for any trouble that may occur by your posting.</p>
+                    </div>
+                    <div class="textarea">
                         <p class="text-ja">最近ストレスかかえていませんか？当サイトはストレス発散サイトです。<br>人の内側にある邪悪な面に焦点を当ててみました。普通、人は何かに対してぼろかすに言うなんてことはしないものです。(...よね？)<br>たとえ言いたくても言わないようにするものです。ということで、当サイトは何かしらいじって楽しみたい人向けのサイトです。<br>溜まった鬱憤を吐き出しましょう！そしてリフレッシュして元気になりましょう！</p>
+                        <p class="text-ja">※投稿による問題が発生した場合、当方では一切責任を一切負いません。自己責任でお願い申し上げます。</p>
                     </div>
             </div>
          </div>
