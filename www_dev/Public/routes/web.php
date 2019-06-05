d80b64b684f360fd1e8b2c2df7bec6fbacc30cf9
佑樹丸林 <maru21k30@gmail.com>
2019-06-05 18:35:21 +0800

    ログイン作成php artisan make:auth実行。 php artisan migrateを実施。

--- www_dev/Public/routes/web.php
+++ www_dev/Public/routes/web.php
@@ -9,10 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+// */
+
+
+// Route::get('/', function () {
+//     return view('login');
+// });
 
 Route::get('/','PageController@index')->name('mazuimeshi.index');       // index.blade.php 表示
 
+Route::group(['middleware' => 'auth'], function() {
 Route::get('/detail','PageController@detail')->name('mazuimeshi.detail');     // detail.blade.php 表示
 Route::get('/login','PageController@login')->name('mazuimeshi.login');     // detail.blade.php 表示
 Route::get('/signup','PageController@signup')->name('mazuimeshi.signup');     // detail.blade.php 表示
+});
+
+Auth::routes(); 
