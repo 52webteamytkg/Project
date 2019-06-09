a40b5c05a1e27c91e2fbe8e499857b8801229702
Kenko <yang_jian_fang@yahoo.co.jp>
2019-06-10 00:13:55 +0800

    Post.php 修正：dislike用Modelの追加

--- www_dev/Public/app/Post.php
+++ www_dev/Public/app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function dislike()
+    {
+        return $this->belongsToMany('App\User', 'dislikes')->withTimestamps();
+    }
+
+
 }
