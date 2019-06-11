5f88f64dfb5dea42212b2daed9904dddd63bf4d1
鍛治多依子 <kajikoo929@gmail.com>
2019-06-11 11:34:37 +0800

    home画面訂正　index.blade.php変更

--- www_dev/Public/resources/views/mazuies/index.blade.php
+++ www_dev/Public/resources/views/mazuies/index.blade.php
@@ -79,8 +79,9 @@
                 @foreach($posts as $post)
                      <div class="col-lg-5 picture">
                         @if($post->img_url)
-                            <img src="{{ str_replace('public/', 'storage/', $post->img_url) }}" width="400" height="400" alt="">
+                            <a class=“” href=“{{ route(‘mazuimeshi.detail’) }}“><img src="{{ str_replace('public/', 'storage/', $post->img_url) }}" width="400" height="400" alt=""></a>
                         @endif
+                        <ul>
                             <li id="created_at">{{ $post['created_at'] }}</li>
                             {{-- dislike btn 機能 --}}
                           {{--   <form method="POST" action="{{route('post.dislike', ['id' => $post['id']]) }} " > --}}
@@ -90,6 +91,7 @@
                             </button></li>
                             {{-- dislike カウント --}}
                             {{-- <span>{{ $diary->dislikes->count() }}</span> --}}
+                        </ul>
                     </div>
                 @endforeach
             </form>
