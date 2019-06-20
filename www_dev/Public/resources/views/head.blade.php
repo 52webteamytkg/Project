3e2b7d61bebba5ed69e3dd3f3fb3b5674cc2bb25
佑樹丸林 <maru21k30@gmail.com>
2019-06-20 10:21:47 +0800

    ファビコンの表示

--- www_dev/Public/resources/views/head.blade.php
+++ www_dev/Public/resources/views/head.blade.php
@@ -9,6 +9,10 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <!-- Font Amatic SC -->
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:700" rel="stylesheet">
+    
+    {{-- ファビコン --}}
+    <link rel="icon" type="image/png" href="/mazui.ico" />
+
 
 {{-- ****** Original Styles ****** --}}
     <link rel="stylesheet" type="text/css" href="/css/style.css">
